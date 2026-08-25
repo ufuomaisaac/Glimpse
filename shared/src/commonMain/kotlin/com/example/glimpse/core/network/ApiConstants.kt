909feb6676817d203bf5e438d16952d781ac37cd
Ufuoma Isaac <ejemudaroufuoma@gmail.com>
2026-08-25 13:34:15 +0100

    removed hardcoded string from AuthApiService

--- shared/src/commonMain/kotlin/com/example/glimpse/core/network/ApiConstants.kt
+++ shared/src/commonMain/kotlin/com/example/glimpse/core/network/ApiConstants.kt
@@ -4,20 +4,15 @@ internal object ApiEndPoints {
     //const val BASE_URL = "https://unadventurous-marsh.outray.app/"
     const val BASE_URL = "https://glimpse.outray.app/"
 
-    const val AUTH_URL = "/v1/client/"
+    private const val AUTH = "/v1/client"
 
     const val GET_ALL_UPLOADS            = "/uploads"
     const val UPLOAD_WITH_FILES          = "/api/v1/uploads/with-files"
     const val GET_RECEIVED_CLUSTER       = "/clusters"
     const val GENERATED_CLUSTER          = "/generated_cluster"
 
-    const val SIGN_IN                    = "/v1/client/sign_ins"
-
-    const val SIGN_UP                    = "/v1/client/sign_ups"
-
-    const val PREPARE_EMAIL_ADDRESS_VERIFICATION           = "/prepare_email_address_verification"
-
-    const val SESSION                    = "/sessions"
+    const val SIGN_IN                    = "$AUTH/sign_ins"
+    const val SIGN_UP                    = "$AUTH/sign_ups"
 
 
 
@@ -26,16 +21,27 @@ internal object ApiEndPoints {
     fun updateUpload(id: String)         = "/uploads/$id"
     fun deleteUpload(id: String)         = "/uploads/$id"
 
-    fun session(sessionId: String)       = "/v1/client/sessions/$sessionId"
+    fun session(sessionId: String)       = "$AUTH/sessions/$sessionId"
 
-    fun prepareEmailVerification(signUpId : String)       = "/v1/client/sign_ups/$signUpId/prepare_email_address_verification"
+    fun prepareEmailVerification(signUpId: String) =
+        "$SIGN_UP/$signUpId/prepare_email_address_verification"
 
-    fun verifyEmail(signUpId: String)        = "/v1/client/sign_ups/$signUpId/attempt_email_address_verification"
-
-    fun signOut(sessionId: String)      = "/v1/client/sessions/$sessionId"
+    fun verifyEmail(signUpId: String) =
+        "$SIGN_UP/$signUpId/attempt_email_address_verification"
 
 
 
     //fun getReceivedClusterById(id: String) = "/clusters/$id"
 
+}
+
+internal object AuthApiConstants {
+    const val LOG_TAG = "AuthApiService"
+    const val IDENTIFIER = "identifier"
+    const val STRATEGY = "strategy"
+    const val PASSWORD = "password"
+    const val EMAIL_ADDRESS = "email_address"
+    const val USERNAME = "username"
+    const val PASSWORD_STRATEGY = "password"
+    const val EMAIL_CODE_STRATEGY = "email_code"
 }
