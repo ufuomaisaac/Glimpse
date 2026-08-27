3f04256f4e873bed96a791cdaf6ba071e255a61b
Ufuoma Isaac <ejemudaroufuoma@gmail.com>
2026-08-27 21:57:17 +0100

    refactored the code in upload and Auth ApiService

--- shared/src/commonMain/kotlin/com/example/glimpse/core/network/service/ClusterApiService.kt
+++ shared/src/commonMain/kotlin/com/example/glimpse/core/network/service/ClusterApiService.kt
@@ -1,6 +1,5 @@
 package com.example.glimpse.core.network.service
 
-import co.touchlab.kermit.Logger
 import com.example.glimpse.core.network.ApiEndPoints
 import com.example.glimpse.core.network.dto.ClusterDto
 import com.example.glimpse.core.network.dto.ShareLinkDto
@@ -10,8 +9,6 @@ import io.ktor.client.request.*
 import io.ktor.client.statement.*
 
 class ClusterApiService(private val client: HttpClient) {
-
-    private val logger = Logger.withTag("ClusterApiService")
 
     suspend fun getClusters(): List<ClusterDto> =
         client.get(ApiEndPoints.GET_RECEIVED_CLUSTER).body()
