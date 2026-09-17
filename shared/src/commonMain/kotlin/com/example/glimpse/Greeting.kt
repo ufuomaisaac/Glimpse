af5d9f3e9f433128108e61824540fa26bee7ba7f
Ufuoma Isaac <ejemudaroufuoma@gmail.com>
2026-09-17 21:43:00 +0100

    Refactored code

--- shared/src/commonMain/kotlin/com/example/glimpse/Greeting.kt
+++
@@ -1,9 +0,0 @@
-package com.example.glimpse
-
-class Greeting {
-    private val platform = getPlatform()
-
-    fun greet(): String {
-        return sayHello(platform.name)
-    }
-}
--- shared/src/commonMain/kotlin/com/example/glimpse/GreetingUtil.kt
+++
@@ -1,4 +0,0 @@
-package com.example.glimpse
-
-fun sayHello(to: String): String =
-    "Hello, $to!"
